9dd61d18735e6f20c74469eca07e4d6adb6792cd
emergency22 <alex.d.murillo@protonmail.com>
2023-07-26 18:34:08 -0500

    npm install of react and react-dom

--- index.js
+++ index.js
@@ -1,106 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+// import React from "react"
+// import ReactDOM from "react-dom"
 
-const myNavBar = (
-    <nav>
-        <h1>Brand Name Here</h1>
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
 
-ReactDOM.createRoot(document.getElementById("root")).render(myNavBar)
-
-// function Navbar() {
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//             <a className="navbar-brand" href="#">Navbar</a>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
-//
-//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//                 <ul className="navbar-nav mr-auto">
-//                     <li className="nav-item active">
-//                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-//                     </li>
-//                     <li className="nav-item">
-//                         <a className="nav-link" href="#">Link</a>
-//                     </li>
-//                     <li className="nav-item dropdown">
-//                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                             Dropdown
-//                         </a>
-//                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                             <a className="dropdown-item" href="#">Action</a>
-//                             <a className="dropdown-item" href="#">Another action</a>
-//                             <div className="dropdown-divider"></div>
-//                             <a className="dropdown-item" href="#">Something else here</a>
-//                         </div>
-//                     </li>
-//                     <li className="nav-item">
-//                         <a className="nav-link disabled" href="#">Disabled</a>
-//                     </li>
-//                 </ul>
-//                 <form className="form-inline my-2 my-lg-0">
-//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//                 </form>
-//             </div>
-//         </nav>
-//     )
-// }
-//
-// ReactDOM.render(
-//     <ul>Chili:<li>Tomatoes</li><li>Beans</li><li>Ground Beef</li></ul>,
-//     document.getElementById("example")
-// )
-//
-//
-// // Challenge: Create your own custom React component!
-// // Call it "MainContent", and have it return a simple
-// // h1 element that says "I'm learning React!"
-//
-// // Afterward, render it below the Navbar (which
-// // you can do inside the ReactDOM.render call below)
-//
-// function MainContent() {
-//     return(
-//     <p>I'm learning React!</p>
-//     )
-// }
-//
-// ReactDOM.render(
-//     <div>
-//         <Navbar />
-//         <MainContent />
-//     </div>,
-//     document.getElementById("root")
-// )
-//
-// const h1 = document.createElement("h1")
-// h1.textContent = "This is an imperative way to program"
-// h1.className = "header"
-// document.getElementById("vanilla").append(h1)
-// //versus...
-// ReactDOM.render(<p>This is a declarative way to program</p>, document.getElementById("notVanilla"))
-//
-//
-//
-//
-//
-// const page = (
-//     <div>
-//         <h1 className="header">Using a const</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// )
-//
-// ReactDOM.render(
-//     page,
-//     document.getElementById("useConst")
-// )
-
+ReactDOM.render(page, document.getElementById("root"))
