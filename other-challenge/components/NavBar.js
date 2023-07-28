72e28a5bd747bbe9fc05452c8d8a13c1820a329d
emergency22 <alex.d.murillo@protonmail.com>
2023-07-28 11:22:49 -0500

    Save point

--- other-challenge/components/NavBar.js
+++ other-challenge/components/NavBar.js
@@ -2,6 +2,10 @@ import React from "react"
 
 export default function NavBar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src="../images/react-icon-small.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
